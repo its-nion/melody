7341284ff3046861d295cd3e2427dc111b0a5c9a
phill <philipp.letschka@uni-ulm.de>
2022-03-03 14:32:32 +0100

    feat: better run script

--- run.sh
+++ run.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
+if ! command -v java &> /dev/null
+then
+    echo "Installing java"
+    sudo apt install openjdk-17-jdk
+fi
+
+if ! command -v gradle &> /dev/null
+then
+  if ! command -v zip &> /dev/null
+  then
+    echo "Installing zip"
+    sudo apt install zip
+  fi
+  if ! command -v unzip &> /dev/null
+  then
+    echo "Installing unzip"
+    sudo apt install unzip
+  fi
+  echo "Installing gradle"
+  sudo curl -s "https://get.sdkman.io" | bash
+  bash "$HOME/.sdkman/bin/sdkman-init.sh"
+  sdk install gradle 7.4
+fi
+
+gradle -q upsertGlobal
 gradle -q execute
