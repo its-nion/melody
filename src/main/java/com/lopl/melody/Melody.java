2b65cf39b0ed55c9bdf943d199065662eed5eab4
its-nion <lukas.olesch@protonmail.com>
2022-03-03 17:53:29 +0100

    feat: added basic graphical interface

--- src/main/java/com/lopl/melody/Melody.java
+++ src/main/java/com/lopl/melody/Melody.java
@@ -13,22 +13,87 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.Compression;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
+import javax.imageio.ImageIO;
 import javax.security.auth.login.LoginException;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
 public class Melody {
   public static JDA manager;
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) throws InterruptedException, AWTException, IOException {
     new Melody();
   }
 
-  private Melody() throws InterruptedException {
+  private Melody() throws InterruptedException, AWTException, IOException {
+        createAndShowGUI();
+
+    /*
     try {
       setup();
     } catch (LoginException e) {
       Logging.error(Melody.class, null, null, "Bot not registered! Head to https://discord.com/developers/applications to register the bot. Open the properties.json to set the bots key afterwards.");
     }
+    */
   }
+
+    public void createAndShowGUI() throws AWTException, IOException {
+      String url = System.getProperty("user.dir") + "\\build\\resources\\main\\bitmap\\melody_transparent.png";
+      ImageIcon image = new ImageIcon(url);
+
+      // General Settings
+      final JFrame frame = new JFrame("Melody");
+      frame.setIconImage(image.getImage());
+      frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+      frame.setResizable(false);
+      frame.setSize(640, 360);
+      frame.setLocationRelativeTo(null);
+
+      // If OS does support SystemTray
+      if (SystemTray.isSupported()) {
+        frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        final SystemTray tray = SystemTray.getSystemTray();
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon icon   = new TrayIcon(image.getImage(), "Melody", popup);
+        icon.setImageAutoSize(true);
+        icon.addMouseListener(new MouseAdapter() {
+          @Override
+          public void mouseClicked(MouseEvent e) {
+            if (e.getButton() == MouseEvent.BUTTON1) {
+              frame.setVisible(true);
+            }
+          }
+        });
+
+        tray.add(icon);
+
+        MenuItem item1 = new MenuItem("Open Melody");
+        item1.addActionListener(new ActionListener() {
+          @Override public void actionPerformed(ActionEvent e) {
+            frame.setVisible(true);
+            frame.setLocationRelativeTo(null);
+          }
+        });
+        MenuItem item2 = new MenuItem("Quit");
+        item2.addActionListener(new ActionListener() {
+          @Override public void actionPerformed(ActionEvent e) {
+            tray.remove(icon);
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.dispose();
+            //System.exit(0);
+          }
+        });
+        popup.add(item1);
+        popup.add(item2);
+      }
+
+      // Makes window visible
+      frame.setVisible(true);
+    }
 
   private void setup() throws LoginException, InterruptedException {
     JDABuilder builder = JDABuilder.createDefault(Token.BOT_TOKEN);
