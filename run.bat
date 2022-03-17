eb4d4b93cb0f2ccf9c384d462d8fe4a9221d805d
phill <philipp.letschka@uni-ulm.de>
2022-03-17 12:36:35 +0100

    feat: run.bat file to easily run melody (with no gui)

--- run.bat
+++ run.bat
@@ -1,1 +1,20 @@
-gradle -q execute
+@echo off
+
+:: checking for file argument
+SET file=%1
+
+:: finding jar file
+IF NOT [%1] == [] goto run
+setlocal enabledelayedexpansion
+for %%f in (builds\*.jar) do (
+    SET fName=%%f
+)
+SET fDir=%~dp0
+SET file="%fDir%%fName%"
+echo "Assumed jar file at: %file%"
+
+:: starting melody
+:run
+echo "Executing java -jar %file%"
+java -jar "%file%" --no-gui
+
