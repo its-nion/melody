0b67b14431fd9b1c36550dd8a3568697b6c0f874
phill <philipp.letschka@uni-ulm.de>
2022-03-17 12:36:25 +0100

    feat: run.sh file to easily run melody (with no gui)

--- run.sh
+++ run.sh
@@ -1,81 +1,33 @@
 #!/bin/bash
 
-while getopts a:f: flag
+# checking for file argument
+while getopts f: flag
 do
     case "${flag}" in
-        a) action=${OPTARG};;
         f) file=${OPTARG};;
         *)
     esac
 done
 
+# checking for java installation and installing if not present
 if ! command -v java &> /dev/null
 then
     echo "Installing java"
     sudo apt install openjdk-15-jdk
 fi
 
-if [ "$action" == "upsertGlobal" ]
-then
-  if [[ ! -n "$file" ]]
-  then
-    index=0
-    for file in ./build/libs/*.jar ; do
-	    f=$file
-	    if [ $index -ge 1 ]
-	    then
-	      break
-	    fi
-	    ((index=index+1))
-    done
-    fName=$(basename "$f")
-    fDir=$(dirname "$f")
-    file="$fDir/$fName"
-    echo "Assumed jar file at: $file"
-  fi
-  echo "Executing java -jar $file"
-  java -jar "$file"
-  exit
-fi
-
-if [ "$action" == "upsertLocal" ]
-then
-  if [[ ! -n "$file" ]]
-  then
-    index=0
-    for file in ./build/libs/*.jar ; do
-	    f=$file
-	    if [ $index -ge 2 ]
-	    then
-	      break
-	    fi
-	    ((index=index+1))
-    done
-    fName=$(basename "$f")
-    fDir=$(dirname "$f")
-    file="$fDir/$fName"
-    echo "Assumed jar file at: $file"
-  fi
-  echo "Executing java -jar $file"
-  java -jar "$file"
-  exit
-fi
-
+# finding jar file
 if [[ ! -n "$file" ]]
 then
-  index=0
-  for file in ./build/libs/*.jar ; do
+  for file in ./builds/*.jar ; do
     f=$file
-    if [ $index -ge 0 ]
-    then
-      break
-    fi
-    ((index=index+1))
   done
   fName=$(basename "$f")
   fDir=$(dirname "$f")
   file="$fDir/$fName"
   echo "Assumed jar file at: $file"
 fi
+
+# starting melody
 echo "Executing java -jar $file"
-java -jar "$file"
+java -jar "$file" --no-gui
